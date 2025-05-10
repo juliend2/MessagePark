7ab1889fe07028b6dc7c5236053704dd8637bcb4
Julien Desrosiers <juliend2@gmail.com>
2025-05-10 09:47:35 -0400

    Remove send() function

--- src/MessagePark/Functions.php
+++ src/MessagePark/Functions.php
@@ -8,15 +8,15 @@ use MessagePark\BaseClass;
 use MessagePark\Types\IntegerClass;
 use MessagePark\Types\FloatClass;
 
-function send($object, $message_name, ...$params) {
-  if (is_object($object) && method_exists($object, $message_name)) {
-    return $object->$message_name(...$params);
-  }
-  if (is_integer($object)) {
-    return send_message_to_integer($object, $message_name, $params);
-  }
-  return new MethodMissingError("Unsupported type for '$object'.");
-}
+// function send($object, $message_name, ...$params) {
+//   if (is_object($object) && method_exists($object, $message_name)) {
+//     return $object->$message_name(...$params);
+//   }
+//   if (is_integer($object)) {
+//     return send_message_to_integer($object, $message_name, $params);
+//   }
+//   throw new MethodMissingError("Unsupported type for '$object'.");
+// }
 
 function send_message_to_integer(int $object, $message_name, $params) {
   $_object = new IntegerClass($object);
--- src/MessagePark/Types/FloatClass.php
+++ src/MessagePark/Types/FloatClass.php
@@ -12,4 +12,3 @@ class FloatClass extends BaseClass {
   }
 }
 
-
